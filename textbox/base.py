78bea1ef1a58f9760d1182b5cb9552337d03a541
Nuffe <carl.j.paulsson@hotmail.com>
2025-06-11 16:07:11 +0200

    text pointer now follows the text written

--- textbox/base.py
+++ textbox/base.py
@@ -5,7 +5,11 @@ pygame.init()
 screen = pygame.display.set_mode((1000, 600))
 font = pygame.font.SysFont(None, 36)
 clock = pygame.time.Clock()
+
 textInput = []
+cursorPos = 0
+positionX = 50
+positionY = 50
 
 running = True
 
@@ -17,31 +21,30 @@ while running:
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_BACKSPACE:
                 textInput.pop()
-            elif event.key == pygame.K_SPACE:
-                textInput.append(' ')
-            elif event.key == pygame.K_TAB:
-                textInput.append('\t')
+                cursorPos -= 1
             elif event.key == pygame.K_c and pygame.key.get_mods() & pygame.KMOD_CTRL:
                 textInput.clear()
+                cursorPos = 0
+            elif event.key == pygame.K_ESCAPE:
+                running = False
             else:
-                keyPressed = pygame.key.name(event.key)
-                textInput.append(keyPressed)
+                character = event.unicode       
+                if character:
+                    textInput.insert(cursorPos, character)
+                    cursorPos += 1
 
-
-    # keyboard event handling
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_ESCAPE]:
-        running = False
-    
     screen.fill((30, 30, 30))
-    
-    if time.time() % 1.2 > 0.6:
-        cursor = pygame.draw.rect(screen, (50, 255, 255), (50, 50, 3, 20))
+
+    if time.time() % 1.2 > 0.6:                              #Flicker time
+        textBeforePoint = "".join(textInput[:cursorPos])     #Grabs all before cursor position
+        pointerX = positionX + font.size(textBeforePoint)[0] #size givs the pixel size of the string and puts caret at the end
+        pointerHeight = font.get_height()
+        pygame.draw.rect(screen, (255,255,255), (pointerX, positionY, 2, pointerHeight))
     
     if len(textInput) > 0:
-        textOutput = ''.join(textInput)
+        textOutput = "".join(textInput)
         key = font.render(textOutput, True, (255, 255, 255))
-        screen.blit(key, (50, 50))
+        screen.blit(key, (positionX, positionY))
 
     clock.tick(60)
     pygame.display.flip()
