a78bcd9cf76ae7aa0c3451d32394cf43e824d407
Nuffe <carl.j.paulsson@hotmail.com>
2025-06-12 12:26:24 +0200

    updated README, Added enterbutton, to create new row

--- README.md
+++ README.md
@@ -2,15 +2,16 @@
 
 # Things to add
 
-# blinking cursor location
-# able for multiple lines 
-#   - when removing last element, jump up a line (and not crash if nothing to jump to)
+# blinking cursor location                                                                          (Cehck)
+# able for multiple lines                                                                           (Check, ish)
+#   - when removing last element, jump up a line (and not crash if nothing to jump to)              (Check)
 #   - fix overflow (if adding mid line surcomvents if statment to end) and single character lines
-# navigate the lines
+# navigate the lines                                                                                (Check)
 # gap buffer
 # Window border, header, buttons (load & save stuff), close button
 # able to load and save stuff
 
-# maybe
+# maybe 
 # ?line numbers
 # ?make open from terminal, maybe a executable
+# higlight line written on, like VS code
--- textbox/base.py
+++ textbox/base.py
@@ -21,10 +21,20 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     
+        # Handle key events
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_BACKSPACE:
-                textLines[pointerY].pop()
-                cursorPos -= 1
+                if( cursorPos > 0 and pointerY < len(textLines)):
+                    textLines[pointerY].pop()
+                    cursorPos -= 1
+                else:
+                    if pointerY > 0:   # Same logic as if backspace at the start of a line
+                        pointerY -= 1
+                        cursorPos = len(textLines[pointerY])
+            elif event.key == pygame.K_RETURN:
+                textLines.append([])                 
+                pointerY += 1
+                cursorPos = 0
             elif event.key == pygame.K_c and pygame.key.get_mods() & pygame.KMOD_CTRL:
                 textLines[pointerY].clear()
                 cursorPos = 0
@@ -32,7 +42,7 @@ while running:
                 running = False
             elif event.key == pygame.K_DOWN:
                 pointerY += 1
-                if pointerY >= len(textLines):
+                if pointerY >= len(textLines): # Creates new line if at the end
                     textLines.append([])
                 cursorPos = min(cursorPos, len(textLines[pointerY]))
             elif event.key == pygame.K_UP:
@@ -43,13 +53,13 @@ while running:
             elif event.key == pygame.K_LEFT:
                 if cursorPos > 0:
                     cursorPos -= 1
-                elif pointerY > 0:
+                elif pointerY > 0: # Jump up if at start
                     pointerY -= 1
-                    cursorPos = len(textLines[pointerY])
+                    cursorPos = len(textLines[pointerY]) 
             elif event.key == pygame.K_RIGHT:
                 if cursorPos < len(textLines[pointerY]):
                     cursorPos += 1
-                elif pointerY < len(textLines) - 1:
+                elif pointerY < len(textLines) - 1: # Jump down if at end
                     pointerY += 1
                     cursorPos = 0
             else:
@@ -62,27 +72,23 @@ while running:
                         cursorPos = 0
                         if pointerY >= len(textLines):
                             textLines.append([])
-                  
-
-
+                
     screen.fill((30, 30, 30))
 
-    textBeforePointLines = "".join(textLines[pointerY][:cursorPos])           #Grabs all before cursor position
-    pointerX = positionX + font.size(textBeforePointLines)[0]                 #X position of the cursor based on pixels before it
+    # Creating the text pointer
+    textBeforePointLines = "".join(textLines[pointerY][:cursorPos]) #Grabs all before cursor position
+    pointerX = positionX + font.size(textBeforePointLines)[0]  #X position of the cursor based on pixels before it
     pointerHeight = font.get_height()
-
-    if time.time() % 1.2 > 0.6:                                               #Flicker time    
+    if time.time() % 1.2 > 0.6:   #Flicker time    
         pygame.draw.rect(screen, (255,255,255), (pointerX,  positionY + pointerY * font.get_height(), 2, pointerHeight))
        
-    
+    # Render the text lines
     for i, line in enumerate(textLines):
         if len(line) == 0:
             continue
         textOutput = "".join(line)
         key = font.render(textOutput, True, (255, 255, 255))
         screen.blit(key, (positionX, positionY + i * font.get_height()))
-
-
 
     clock.tick(60)
     pygame.display.flip()
