aba198f8c06bd4a45b014b1e7844ccae538ae427
Nuffe <carl.j.paulsson@hotmail.com>
2025-06-12 09:20:25 +0200

    start on implementation for multiline. Changed the textinput array to now be two dimentional (one array for each line). Also started with some line navigation, co-pilot spit outsomething that works but will look into it more.

--- textbox/base.py
+++ textbox/base.py
@@ -7,9 +7,12 @@ font = pygame.font.SysFont(None, 36)
 clock = pygame.time.Clock()
 
 textInput = []
+textLines = [[]] 
 cursorPos = 0
 positionX = 50
 positionY = 50
+pointerX = 0
+pointerY = 0
 
 running = True
 
@@ -20,31 +23,66 @@ while running:
     
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_BACKSPACE:
-                textInput.pop()
+                textLines[pointerY].pop()
                 cursorPos -= 1
             elif event.key == pygame.K_c and pygame.key.get_mods() & pygame.KMOD_CTRL:
-                textInput.clear()
+                textLines[pointerY].clear()
                 cursorPos = 0
             elif event.key == pygame.K_ESCAPE:
                 running = False
+            elif event.key == pygame.K_DOWN:
+                pointerY += 1
+                if pointerY >= len(textLines):
+                    textLines.append([])
+                cursorPos = min(cursorPos, len(textLines[pointerY]))
+            elif event.key == pygame.K_UP:
+                pointerY -= 1
+                if pointerY < 0:
+                    pointerY = 0
+                cursorPos = min(cursorPos, len(textLines[pointerY]))
+            elif event.key == pygame.K_LEFT:
+                if cursorPos > 0:
+                    cursorPos -= 1
+                elif pointerY > 0:
+                    pointerY -= 1
+                    cursorPos = len(textLines[pointerY])
+            elif event.key == pygame.K_RIGHT:
+                if cursorPos < len(textLines[pointerY]):
+                    cursorPos += 1
+                elif pointerY < len(textLines) - 1:
+                    pointerY += 1
+                    cursorPos = 0
             else:
                 character = event.unicode       
                 if character:
-                    textInput.insert(cursorPos, character)
+                    textLines[pointerY].insert(cursorPos, character)
                     cursorPos += 1
+                    if pointerX > 500:                      
+                        pointerY += 1
+                        cursorPos = 0
+                        if pointerY >= len(textLines):
+                            textLines.append([])
+                  
+
 
     screen.fill((30, 30, 30))
 
-    if time.time() % 1.2 > 0.6:                              #Flicker time
-        textBeforePoint = "".join(textInput[:cursorPos])     #Grabs all before cursor position
-        pointerX = positionX + font.size(textBeforePoint)[0] #size givs the pixel size of the string and puts caret at the end
-        pointerHeight = font.get_height()
-        pygame.draw.rect(screen, (255,255,255), (pointerX, positionY, 2, pointerHeight))
+    textBeforePointLines = "".join(textLines[pointerY][:cursorPos])           #Grabs all before cursor position
+    pointerX = positionX + font.size(textBeforePointLines)[0]                 #X position of the cursor based on pixels before it
+    pointerHeight = font.get_height()
+
+    if time.time() % 1.2 > 0.6:                                               #Flicker time    
+        pygame.draw.rect(screen, (255,255,255), (pointerX,  positionY + pointerY * font.get_height(), 2, pointerHeight))
+       
     
-    if len(textInput) > 0:
-        textOutput = "".join(textInput)
+    for i, line in enumerate(textLines):
+        if len(line) == 0:
+            continue
+        textOutput = "".join(line)
         key = font.render(textOutput, True, (255, 255, 255))
-        screen.blit(key, (positionX, positionY))
+        screen.blit(key, (positionX, positionY + i * font.get_height()))
+
+
 
     clock.tick(60)
     pygame.display.flip()
