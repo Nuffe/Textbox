b7b5f04add7cec7aed3e86a6ec05b9b988457e48
Nuffe <carl.j.paulsson@hotmail.com>
2025-06-24 08:53:45 +0200

    Changed linked list. Added upkeep on head, tail and count inside. Removed ununsed functions.

--- linkedList.py
+++ linkedList.py
@@ -1,104 +1,55 @@
-
-
 class Node:
     def __init__(self, data):
         self.data = data
-        self.next = None
         self.prev = None
-    
-def traverse(head):
-        current = head
-        while current:
-            print(current.data, end=' ')
-            current = current.next
-        print("None")
+        self.next = None
 
-def insert_at_beginning(head, data):
-        new_node = Node(data)
-        new_node.next = head
-        if head:
-            head.prev = new_node
-        return new_node
-    
-def insert_after_node(node, data):
-    if node is None:
-        print("Error: The given node is None")
-        return
+class LineList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.size = 0
 
-    new_node = Node(data)
-    new_node.prev = node
-    new_node.next = node.next
-
-    if node.next:
-        node.next.prev = new_node
-
-    node.next = new_node
-
-def insert_at_end(head, data):
-    # Insert a new node at the end of the doubly linked list
-    new_node = Node(data)
-    if head is None:
+    def append(self, buffer):
+        new_node = Node(buffer)
+        if not self.head:
+            self.head = self.tail = new_node
+        else:
+            new_node.prev = self.tail
+            self.tail.next = new_node
+            self.tail = new_node
+        self.size += 1
         return new_node
 
-    current = head
-    while current.next:
-        current = current.next
+    def insert_after(self, node, buffer):
+        if node is None:
+            print("Error: The given node is None")
+            return
+        new_node = Node(buffer)
+        new_node.prev = node
+        new_node.next = node.next
+        if node.next:
+            node.next.prev = new_node
+        else:
+            # we are at the tail and update it
+            self.tail      = new_node
+        node.next         = new_node
+        self.size        += 1
+        return new_node
 
-    current.next = new_node
-    new_node.prev = current
-    return head
+    def remove(self, node):
+        if node.prev:
+            node.prev.next = node.next
+        else:
+            # if no prev, then we at head
+            self.head = node.next
 
-def delete_at_position(head, position):
-    # Delete the node at a given position from the doubly linked list
-    if head is None:
-        print("Doubly linked list is empty")
-        return None
+        # need two "if else" because head and tail can be the same node
+        if node.next:
+            node.next.prev = node.prev
+        else:
+            # if no next, we at tail
+            self.tail = node.prev
 
-    if position < 0:
-        print("Invalid position")
-        return head
-
-    if position == 0:
-        if head.next:
-            head.next.prev = None
-        return head.next
-
-    current = head
-    count = 0
-    while current and count < position:
-        current = current.next
-        count += 1
-
-    if current is None:
-        print("Position out of range")
-        return head
-
-    if current.next:
-        current.next.prev = current.prev
-    if current.prev:
-        current.prev.next = current.next
-
-    del current
-    return head
-
-def delete_at_end(head):
-    # Delete the last node from the end of the doubly linked list
-    if head is None:
-        print("Doubly linked list is empty")
-        return None
-
-    if head.next is None:
-        return None
-
-    current = head
-    while current.next.next:
-        current = current.next
-
-    del_node = current.next
-    current.next = None
-    del del_node
-    return head
-
-
-
-
+        node.prev = node.next = None
+        self.size -= 1
