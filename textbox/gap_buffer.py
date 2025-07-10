df18fe7225ff058c40d2f16f502ff7e50d9c54c3
Carl <Carl.j.paulsson@hotmail.com>
2025-07-10 11:29:49 +0200

    Buffer update
    Noticed that when adding mid word sometimes removed the remaining word. Turned out to be the buffer working oddly. Simplifying the growth part seam to fix it.

--- textbox/gap_buffer.py
+++ textbox/gap_buffer.py
@@ -2,37 +2,36 @@
 class gapBuffer:
     def __init__(self, size):
         self.size = size
-        self.gap_size = 5
+        self.gap_size = 10
         self.buffer = ['_'] * self.size
         self.gap_start = 0
-        self.gap_end = self.gap_start + self.gap_size
-       # print(self.buffer)
+        self.gap_end = self.gap_size
 
+    def grow(self, amount, position):
+            # Increasing the size of the gap
+            self.buffer[position:position] = ['_'] * amount
+            self.size += amount
+            self.gap_end += amount
+            self.gap_size += amount
 
-    # Grow the gap at position, new array is made and old data is copied over
-    def grow(self, growthSize: int, position):
-        a = self.buffer[position:self.size]
-        self.buffer[position:position+growthSize] = ['_'] * growthSize
-        self.buffer[position+growthSize:position+growthSize + self.size - position] = a
-
-        self.size += growthSize
-        self.gap_size += growthSize
-        self.gap_end = self.gap_start + self.gap_size
-
-    # moves the gap right by changing the character to "", array size is not changed
     def right(self, position):
         while position > self.gap_start:
+            if self.gap_end >= len(self.buffer):
+                break
             self.buffer[self.gap_start] = self.buffer[self.gap_end]
-            self.buffer[self.gap_end] = '_' 
+            self.buffer[self.gap_end] = '_'
+
             self.gap_start += 1
-            self.gap_end   += 1
+            self.gap_end += 1
+            print("moving right")
 
     def left(self, position):
          while position < self.gap_start:
-            self.gap_end   -= 1
+            self.gap_end -= 1
             self.gap_start -= 1  
             self.buffer[self.gap_end] = self.buffer[self.gap_start]
             self.buffer[self.gap_start] = '_'
+            print("moving left")
 
     # way to keep position and gap in sync
     def move_position(self, position):
@@ -43,28 +42,33 @@ class gapBuffer:
         else:
             self.right(position)
 
+    def insert(self, position, input):
+        if position != self.gap_start:
+             self.move_position(position)
+        for char in input:
+            if self.gap_start >= self.gap_end:
+                self.grow(5, self.gap_start)
+            self.buffer[self.gap_start] = char
+            self.gap_start += 1
+            self.gap_size -= 1
+            self.gap_end = self.gap_start + self.gap_size
 
-    def insert(self, position, char):
-        if self.gap_size == 0:
-            self.grow(5, position)
-        self.move_position(position)
-        self.buffer[self.gap_start] = char
-        self.gap_start += 1
-        self.gap_size -= 1
-        self.gap_end = self.gap_start + self.gap_size
-
+            print(self.buffer)
 
     # simular to move left, but increases the gap size
-    def delete(self, position):         
-        self.move_position(position)
+    def delete(self, position):
+        if position != self.gap_start: # Always be positiond at the gap_start
+            self.move_position(position)
+
+        if self.gap_start == 0: # Prevent deletion before the start of the buffer
+            return None
+
         self.gap_start -= 1
         deleted = self.buffer[self.gap_start]
         self.buffer[self.gap_start] = '_'  
         self.gap_size += 1
         self.gap_end = self.gap_start + self.gap_size
         return deleted
-    
-
 
     # Returns the text content, excluding the gap
     def textContent(self):
